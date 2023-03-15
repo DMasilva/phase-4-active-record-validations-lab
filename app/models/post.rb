4826b88fa718885b5d60d10ce0fb450f9eee3a90
David <david.owuor@student.moringaschool.com>
2023-03-15 23:41:22 +0300

    first commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: {maximum: 250}
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  validate :click_baity
+
+
+  def click_baity
+  
+  end
 end
